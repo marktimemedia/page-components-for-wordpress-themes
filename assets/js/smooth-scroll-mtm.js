ab3463a788152adafb0deccd6969463a8bb2dd88
Michelle Schulp <michelle@marktimemedia.com>
2016-05-29 10:39:17 -0500

    update # selector in jquery to work again

--- assets/js/smooth-scroll-mtm.js
+++ assets/js/smooth-scroll-mtm.js
@@ -2,7 +2,7 @@
 // Smooth Scroll to anchor
 // From https://css-tricks.com/snippets/jquery/smooth-scrolling/
 
-  $('a[href*=#]:not([href=#])').click(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
