5f60f14a6d4326edd0a756cef667772da3b6b5c0
Michelle Schulp <michelle@marktimemedia.com>
2016-12-19 19:55:46 -0600

    fix for wp 4.7 page template changes

--- lib/class-mtm-component-template.php
+++ lib/class-mtm-component-template.php
@@ -52,12 +52,23 @@ if ( ! class_exists( 'Mtm_Component_Templates' ) )  {
 
                     $this->templates = array();
 
-
                     // Add a filter to the attributes metabox to inject template into the cache.
-                    add_filter(
-    					'page_attributes_dropdown_pages_args',
-    					 array( $this, 'mtm_register_project_templates' ) 
-    				);
+                    if ( version_compare( floatval( get_bloginfo( 'version' ) ), '4.7', '<' ) ) {
+
+                        // 4.6 and older
+                        add_filter(
+                            'page_attributes_dropdown_pages_args',
+                            array( $this, 'mtm_register_project_templates' )
+                        );
+
+                    } else {
+
+                        // Add a filter to the wp 4.7 version attributes metabox
+                        add_filter(
+                            'theme_page_templates', array( $this, 'mtm_add_new_template' )
+                        );
+
+                    }
 
 
                     // Add a filter to the save post to inject out template into the page cache
@@ -85,6 +96,15 @@ if ( ! class_exists( 'Mtm_Component_Templates' ) )  {
                     );
     				
             } 
+
+            /**
+             * Adds our template to the page dropdown for v4.7+
+             *
+             */
+            public function mtm_add_new_template( $posts_templates ) {
+                $posts_templates = array_merge( $posts_templates, $this->templates );
+                return $posts_templates;
+            }
 
 
             /**
@@ -116,7 +136,6 @@ if ( ! class_exists( 'Mtm_Component_Templates' ) )  {
                     wp_cache_add( $cache_key, $templates, 'themes', 1800 );
 
                     return $atts;
-
             } 
 
             /**
