e82eadd0a3c967f3405673a905dd5a82bdc308f2
Michelle Schulp <michelle@marktimemedia.com>
2017-06-02 15:47:20 -0500

    fix template issue with mixing if/else formats, oops

--- templates/template-news.php
+++ templates/template-news.php
@@ -6,18 +6,18 @@ Template Name: News Page Builder
 mtm_load_wrap_header();
 
 // HIDES ACF FIELDS for use with WP password protect feature
-if( !post_password_required( $post )):
+if( !post_password_required( $post )) :
 
 	// News Homepage
 	mtm_get_template_part( 'mtm-layout', 'home-news' ); 
 
 	// Single Scroll
-	if( _get_field( 'mtm_enable_single_scroll_page' ) ) {
+	if( _get_field( 'mtm_enable_single_scroll_page' ) ) :
 
 		mtm_get_template_part( 'mtm-logic', 'single-scroll' );
-	}
+	endif;
 
-else: ?>
+else : ?>
 	<section id="<?php echo $post->post_name; ?>">
 
 		<?php echo get_the_password_form(); ?>
