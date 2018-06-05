046b40582c2cb2b40cb8750e531b1527c080350a
Michelle Schulp <michelle@marktimemedia.com>
2018-06-05 11:03:28 -0400

    adding anchor links to titles

--- templates/mtm-logic-modules.php
+++ templates/mtm-logic-modules.php
@@ -18,27 +18,30 @@ $j=1; ?>
 	    	<?php the_row(); ?>
 
 			<?php // Logos
-			if( "mtm_module_logo_showcase" == get_row_layout() ) : ?>			
+			if( "mtm_module_logo_showcase" == get_row_layout() ) : 
+				$anchor = get_sub_field( 'mtm_logo_title' ) ? sanitize_title_with_dashes( get_sub_field( 'mtm_logo_title' ) ) : '' ; // title to anchor tag ?>
 				
-				<div class="<?php echo get_row_layout(); ?>">
+				<div class="<?php echo get_row_layout(); ?>" id="<?php echo( esc_html( $anchor ) ); ?>">
 
 					<?php mtm_get_template_part( 'mtm-module', 'logos' ); ?>
 
 				</div>
 
 			<?php // Call To Action
-			elseif( "mtm_module_call_to_action" == get_row_layout() ) : ?>
+			elseif( "mtm_module_call_to_action" == get_row_layout() ) : 
+				$anchor = get_sub_field( 'mtm_cta_headline' ) ? sanitize_title_with_dashes( get_sub_field( 'mtm_cta_headline' ) ) : '' ; // title to anchor tag ?>
 				
-				<div class="<?php echo get_row_layout(); ?>">
+				<div class="<?php echo get_row_layout(); ?>" id="<?php echo( esc_html( $anchor ) ); ?>">
 
 					<?php mtm_get_template_part( 'mtm-module', 'call-to-action' ); ?>
 
 				</div>
 
 			<?php // Feature Boxes
-			elseif( "mtm_module_feature_boxes" == get_row_layout() ) : ?>
+			elseif( "mtm_module_feature_boxes" == get_row_layout() ) : 
+				$anchor = get_sub_field( 'mtm_text_area_title' ) ? sanitize_title_with_dashes( get_sub_field( 'mtm_text_area_title' ) ) : '' ; // title to anchor tag ?>
 				
-				<div class="<?php echo get_row_layout(); ?>">
+				<div class="<?php echo get_row_layout(); ?>" id="<?php echo( esc_html( $anchor ) ); ?>">
 
 					<?php mtm_get_template_part( 'mtm-module', 'featured-content' ); ?>
 
@@ -72,72 +75,80 @@ $j=1; ?>
 				</div>
 
 			<?php // Single Content
-			elseif( "mtm_module_single_content_area" == get_row_layout() ) : ?>
+			elseif( "mtm_module_single_content_area" == get_row_layout() ) : 
+				$anchor = get_sub_field( 'mtm_text_area_title' ) ? sanitize_title_with_dashes( get_sub_field( 'mtm_text_area_title' ) ) : '' ; // title to anchor tag ?>
 				
-				<div class="<?php echo get_row_layout(); ?>">
+				<div class="<?php echo get_row_layout(); ?>" id="<?php echo( esc_html( $anchor ) ); ?>">
 
 					<?php mtm_get_template_part( 'mtm-module', 'text' ); ?>
 
 				</div>
 
 			<?php // Dual Content
-			elseif( "mtm_module_dual_content_area" == get_row_layout() ) : ?>
+			elseif( "mtm_module_dual_content_area" == get_row_layout() ) : 
+				$anchor = get_sub_field( 'mtm_text_area_title' ) ? sanitize_title_with_dashes( get_sub_field( 'mtm_text_area_title' ) ) : '' ; // title to anchor tag ?>
 				
-				<div class="<?php echo get_row_layout(); ?>">
+				<div class="<?php echo get_row_layout(); ?>" id="<?php echo( esc_html( $anchor ) ); ?>">
 
 					<?php mtm_get_template_part( 'mtm-module', 'text' ); ?>
 
 				</div>
 
 			<?php // Content + Callout
-			elseif( "mtm_module_content_callout" == get_row_layout() ) : ?>
+			elseif( "mtm_module_content_callout" == get_row_layout() ) : 
+				$anchor = get_sub_field( 'mtm_text_area_title' ) ? sanitize_title_with_dashes( get_sub_field( 'mtm_text_area_title' ) ) : '' ; // title to anchor tag ?>
 				
-				<div class="<?php echo get_row_layout(); ?>">
+				<div class="<?php echo get_row_layout(); ?>" id="<?php echo( esc_html( $anchor ) ); ?>">
 
 					<?php mtm_get_template_part( 'mtm-module', 'text' ); ?>
 
 				</div>
 
 			<?php // Widgets
-			elseif( "mtm_module_widget_area" == get_row_layout() ) : ?>
+			elseif( "mtm_module_widget_area" == get_row_layout() ) : 
+				$anchor = get_sub_field( 'mtm_widget_area_title' ) ? sanitize_title_with_dashes( get_sub_field( 'mtm_widget_area_title' ) ) : '' ; // title to anchor tag ?>
 				
-				<div class="<?php echo get_row_layout(); ?>">
+				<div class="<?php echo get_row_layout(); ?>" id="<?php echo( esc_html( $anchor ) ); ?>">
 
 					<?php mtm_get_template_part( 'mtm-module', 'widget-area' ); ?>
 
 				</div>
 
 			<?php // List Manual
-			elseif( "mtm_module_listgrid" == get_row_layout() ) : ?>
+			elseif( "mtm_module_listgrid" == get_row_layout() ) : 
+				$anchor = get_sub_field( 'mtm_list_title' ) ? sanitize_title_with_dashes( get_sub_field( 'mtm_list_title' ) ) : '' ; // title to anchor tag ?>
 				
-				<div class="<?php echo get_row_layout(); ?>">
+				<div class="<?php echo get_row_layout(); ?>" id="<?php echo( esc_html( $anchor ) ); ?>">
 
 					<?php mtm_get_template_part( 'mtm-module', 'list' ); ?>
 
 				</div>
 
 			<?php // Grid Manual
-			elseif( "mtm_module_gridlist" == get_row_layout() ) : ?>
+			elseif( "mtm_module_gridlist" == get_row_layout() ) : 
+				$anchor = get_sub_field( 'mtm_list_title' ) ? sanitize_title_with_dashes( get_sub_field( 'mtm_list_title' ) ) : '' ; // title to anchor tag ?>
 				
-				<div class="<?php echo get_row_layout(); ?>">
+				<div class="<?php echo get_row_layout(); ?>" id="<?php echo( esc_html( $anchor ) ); ?>">
 
 					<?php mtm_get_template_part( 'mtm-module', 'grid' ); ?>
 
 				</div>
 
 			<?php // List Posts
-			elseif( "mtm_module_listgrid_posts" == get_row_layout() ) : ?>
+			elseif( "mtm_module_listgrid_posts" == get_row_layout() ) : 
+				$anchor = get_sub_field( 'mtm_list_title' ) ? sanitize_title_with_dashes( get_sub_field( 'mtm_list_title' ) ) : '' ; // title to anchor tag ?>
 				
-				<div class="<?php echo get_row_layout(); ?>">
+				<div class="<?php echo get_row_layout(); ?>" id="<?php echo( esc_html( $anchor ) ); ?>">
 
 					<?php mtm_get_template_part( 'mtm-module', 'list' ); ?>
 
 				</div>
 
 			<?php // Grid Posts
-			elseif( "mtm_module_gridlist_posts" == get_row_layout() ) : ?>
+			elseif( "mtm_module_gridlist_posts" == get_row_layout() ) : 
+				$anchor = get_sub_field( 'mtm_list_title' ) ? sanitize_title_with_dashes( get_sub_field( 'mtm_list_title' ) ) : '' ; // title to anchor tag ?>
 				
-				<div class="<?php echo get_row_layout(); ?>">
+				<div class="<?php echo get_row_layout(); ?>" id="<?php echo( esc_html( $anchor ) ); ?>">
 
 					<?php mtm_get_template_part( 'mtm-module', 'grid' ); ?>
 
--- templates/mtm-module-hero-media.php
+++ templates/mtm-module-hero-media.php
@@ -5,7 +5,7 @@ if( "Video" == get_sub_field( 'mtm_hero_select_media' ) ) : ?>
 	
 	<?php mtm_get_template_part( 'mtm-content', 'hero-video' ); ?>
 
-<?php // Other
+<?php // Other TODO: Unique Media Template
 elseif( "Other Media" == get_sub_field( 'mtm_hero_select_media' ) ) : ?>
 	
 	<?php mtm_get_template_part( 'mtm-content', 'hero-video' ); ?>
