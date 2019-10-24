787ca786426347eb315f894223ad16bc9f41a30f
Michelle Schulp <michelle@marktimemedia.com>
2019-10-24 17:30:43 -0500

    Conditionally disable block editor
    Disable block editor on component field page templates so we don't need the classic editor plugin

--- lib/mtm-config.php
+++ lib/mtm-config.php
@@ -98,3 +98,47 @@ if( get_field( 'mtm_register_sidebars', 'option' ) ) {
         add_action( 'widgets_init', 'mtm_template_sidebar_modular' );
     }
 }
+
+
+/**
+ * Templates and Page IDs without editor
+ *
+ */
+function mtm_disable_editor( $id = false ) {
+
+	$excluded_templates = array(
+		'../templates/template-home.php',
+		'../templates/template-components.php',
+		'../templates/template-modules.php',
+		'../templates/template-news.php'
+	);
+
+	$excluded_ids = array(
+		// get_option( 'page_on_front' )
+	);
+
+	if( empty( $id ) )
+		return false;
+
+	$id = intval( $id );
+	$template = get_page_template_slug( $id );
+
+	return in_array( $id, $excluded_ids ) || in_array( $template, $excluded_templates );
+}
+  /**
+ * Disable Gutenberg by template
+ *
+ */
+function mtm_disable_gutenberg( $can_edit, $post_type ) {
+
+	if( ! ( is_admin() && !empty( $_GET['post'] ) ) )
+		return $can_edit;
+
+	if( mtm_disable_editor( $_GET['post'] ) )
+		$can_edit = false;
+
+	return $can_edit;
+
+}
+add_filter( 'gutenberg_can_edit_post_type', 'mtm_disable_gutenberg', 10, 2 );
+add_filter( 'use_block_editor_for_post_type', 'mtm_disable_gutenberg', 10, 2 );
