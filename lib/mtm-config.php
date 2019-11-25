aab1b739b78b733475ec71d91baf6adee1cc2a7a
Michelle Schulp <michelle@marktimemedia.com>
2019-11-25 15:27:15 -0600

    compatibility fixes
    mostly making sure funtions check if they exist

--- lib/mtm-config.php
+++ lib/mtm-config.php
@@ -7,33 +7,39 @@
 /**
 * Make page components compatible with non Roots/Sage based themes by calling header and footer
 */
-function mtm_load_wrap() {
-	if ( class_exists( 'SageWrapping' ) || class_exists( 'Spring_Wrapping' ) ) {
-		return false;
-	}
+if ( !function_exists( 'mtm_load_wrap' ) ) {
+	function mtm_load_wrap() {
+		if ( class_exists( 'SageWrapping' ) || class_exists( 'Spring_Wrapping' ) ) {
+			return false;
+		}
 
-	return true;
+		return true;
+	}
+	add_action( 'after_setup_theme', 'mtm_load_wrap' );
 }
-add_action( 'after_setup_theme', 'mtm_load_wrap' );
 
 /**
 * Load header on standard themes
 */
-function mtm_load_wrap_header() {
+if (!function_exists( 'mtm_load_wrap_header' ) ) {
+	function mtm_load_wrap_header() {
 
-	if ( mtm_load_wrap() ) {
-		get_header();
-		echo '<main id="main" class="site-main" role="main">';
+		if ( mtm_load_wrap() ) {
+			get_header();
+			echo '<main id="main" class="site-main" role="main">';
+		}
 	}
 }
 /**
 * Load footer on standard themes
 */
-function mtm_load_wrap_footer() {
+if (!function_exists( 'mtm_load_wrap_footer' ) ) {
+	function mtm_load_wrap_footer() {
 
-	if ( mtm_load_wrap() ) {
-		echo '</main>';
-		get_footer();
+		if ( mtm_load_wrap() ) {
+			echo '</main>';
+			get_footer();
+		}
 	}
 }
 
